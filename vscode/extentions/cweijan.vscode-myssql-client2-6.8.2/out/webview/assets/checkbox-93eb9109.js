0de7e7f274a57a8b556886230438267d1240b6df
kcwww <cwkim0321@gmail.com>
2023-12-20 16:24:18 +0900

    update extension

--- vscode/extentions/cweijan.vscode-myssql-client2-6.8.2/out/webview/assets/checkbox-93eb9109.js
+++
@@ -1,1 +0,0 @@
-import{E as o}from"./vueConfig-c0688b3b.js";import{r}from"./checkbox-4622959d.js";var t=r();const s=o(t);export{s as _};
---
+++ vscode/extentions/cweijan.vscode-myssql-client2-6.9.5/out/webview/assets/checkbox-afe2fab5.js
@@ -0,0 +1,1 @@
+import{s as o}from"./pinia-1d7b60d7.js";import{r}from"./checkbox-52e6312f.js";var t=r();const p=o(t);export{p as _};
