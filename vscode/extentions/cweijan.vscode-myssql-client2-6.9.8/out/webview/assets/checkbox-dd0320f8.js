daa9fdf8480ca643d3752e57299f1c8f99fbaab3
kcwww <cwkim0321@gmail.com>
2024-01-16 13:31:30 +0900

    update extension

--- vscode/extentions/cweijan.vscode-myssql-client2-6.9.8/out/webview/assets/checkbox-dd0320f8.js
+++
@@ -1,1 +0,0 @@
-import{t as o}from"./pinia-017113cf.js";import{r}from"./checkbox-c721a2e2.js";var t=r();const s=o(t);export{s as _};
--- vscode/extentions/cweijan.vscode-myssql-client2-6.9.8/out/webview/assets/storeUtil-368bd2b4.js
+++
@@ -1,1 +0,0 @@
-import{t as n}from"./pinia-017113cf.js";import{r as c}from"./checkbox-group-613b58ea.js";var i=c();const s=n(i),e=/^(\$|_|vm)/i;function f(t,r){for(const o of Object.keys(t))o.match(e)||r[o]===void 0&&(r[o]=null);t.$patch(r)}function m(t){return JSON.parse(JSON.stringify(t,(r,o)=>r.match(e)?null:o))}export{s as _,m as c,f as r};
